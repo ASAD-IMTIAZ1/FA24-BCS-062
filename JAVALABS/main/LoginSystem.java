697717bbb670a0f8bd3e858218bf3575a222ac27
ASAD-IMTIAZ1 <asadimtiaz867@gmail.com>
2025-02-27 21:14:33 +0500

    Add files via upload

---
+++ JAVALABS/main/LoginSystem.java
@@ -0,0 +1,28 @@
+package main;
+import java.io.Console;
+import users.UserManager;
+import java.util.Scanner;
+public class LoginSystem{
+
+	public static void main(String args[]){
+
+String name;
+String Password;
+
+UserManager a = new UserManager();
+
+Scanner Pass=new Scanner(System.in);
+Console c=System.console();
+
+System.io.printf("Enter Name : ");
+name =c.readLine();
+System.io.printf("Enter Password");
+Password=c.readLine();
+
+}
+
+
+
+
+}
+
---
+++ JAVALABS/users/UserManager.java
@@ -0,0 +1,18 @@
+package users;
+
+import java.io.Console;
+
+public class UserManager{
+
+
+	String name="admin";
+	String password="1234";
+	String InputName;
+	String InputPass;
+
+
+static class Inner{
+
+}
+
+}

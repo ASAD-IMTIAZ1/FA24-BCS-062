56b527d5f959cbafd982bc2a8335d6de62b103a0
ASAD-IMTIAZ1 <asadimtiaz867@gmail.com>
2025-02-27 20:51:46 +0500

    Delete FA24BCSB.java

--- FA24BCSB.java
+++
@@ -1,9 +0,0 @@
-public class FA24BCSB{
-  public static void main(String args[]){
-       System.out.print("FIRST JAVA");
-System.out.printf("FIRST JAVA");
-System.out.printIn("FIRST JAVA");
-
-}
-
-}
